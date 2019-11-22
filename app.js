64f94f22b29e91afd9ddf21680bc9999dd457eee
AhmadForhat <ahmad.forhat@gmail.com>
2019-11-22 00:33:28 -0300

    Mudança do título do projeto

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res, next) {
     res.render('index', {
-        title: "Live University",
+        title: "Node With Ejs",
         version: "0.0.0",
         dev: "Ahmad Forhat",
     });
