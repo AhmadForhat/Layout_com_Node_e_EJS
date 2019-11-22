93fb3698761a70947f4d6e840e02d6918c6b97f9
AhmadForhat <ahmad.forhat@gmail.com>
2019-11-22 00:14:11 -0300

    Construção básica da estrutura Node

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+// Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next) {
+    res.render('index', {
+        title: "Live University",
+        version: "0.0.0",
+        dev: "Ahmad Forhat",
+    });
+});
+
+
+app.listen(port, err => {
+    console.log(`Server is listening on ${port}`);
+});
