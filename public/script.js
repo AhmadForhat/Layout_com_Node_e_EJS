c56faaeb7a2f44a3126abe35da3efea7b1c052e7
AhmadForhat <ahmad.forhat@gmail.com>
2019-11-22 09:25:09 -0300

    Adição da seleção dos itens A e B no dropdown

--- public/script.js
+++ public/script.js
@@ -1,16 +1,23 @@
 const options = document.querySelector(".options");
 const dropdown = document.querySelector(".dropdown");
-const textDropdown = document.querySelector(".dropdown p");
+const textDropdown = document.querySelector(".dropdown h2");
+const dropdownOption1 = document.querySelector(".dropdown p");
+const dropdownOption2 = document.querySelector(".opcao");
 const arrow2 = document.querySelector(".arrow2");
 const home = document.querySelector(".home");
+const home1 = document.querySelector(".homeA")
+const home2 = document.querySelector(".homeB")
 
 options.addEventListener("click", function(){
     dropdown.classList.toggle('ativo');
     dropdown.classList.remove('ativado');
+    textDropdownOption.classList.add('none2');
 })
 
 textDropdown.addEventListener("click", function(){
     dropdown.classList.toggle('ativado');
+    dropdownOption1.classList.toggle('ativado');
+    dropdownOption2.classList.toggle('ativado');
 })
 
 home.addEventListener("click", function(){
