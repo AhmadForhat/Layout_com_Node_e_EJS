933b070923e35c67632c3f9e74911991162d2436
AhmadForhat <ahmad.forhat@gmail.com>
2019-11-22 08:43:24 -0300

    Adição da retirada do dropdown com click fora do quadrado

--- public/script.js
+++ public/script.js
@@ -1,13 +1,19 @@
-const botao = document.querySelector(".botao");
+const options = document.querySelector(".options");
 const dropdown = document.querySelector(".dropdown");
 const textDropdown = document.querySelector(".dropdown p");
 const arrow2 = document.querySelector(".arrow2");
+const home = document.querySelector(".home");
 
-botao.addEventListener("click", function(){
+options.addEventListener("click", function(){
     dropdown.classList.toggle('ativo');
     dropdown.classList.remove('ativado');
 })
 
 textDropdown.addEventListener("click", function(){
     dropdown.classList.toggle('ativado');
+})
+
+home.addEventListener("click", function(){
+    dropdown.classList.remove('ativo');
+    dropdown.classList.remove('ativado');
 })
