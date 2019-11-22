011d552000f51ec9c6b6f3ababa56f1dd672719e
AhmadForhat <ahmad.forhat@gmail.com>
2019-11-22 12:08:22 -0300

    Adição da interação com o input para alterar a quantidade de itens

--- public/script.js
+++ public/script.js
@@ -5,10 +5,14 @@ const dropdownOption1 = document.querySelector(".dropdown p");
 const dropdownOption2 = document.querySelector(".opcao");
 const arrow2 = document.querySelector(".arrow2");
 const home = document.querySelector(".home");
+const homeAA = document.querySelector(".homeAA");
 const homeA = document.querySelector(".homeA");
+const homeBB = document.querySelector(".homeBB");
 const homeB = document.querySelector(".homeB");
 const botao = document.querySelector(".botao");
-const itens = document.querySelector(".itens")
+const itens = document.querySelector(".itens");
+const setas = document.querySelector(".flex2");
+const input = document.getElementById("numero");
 
 options.addEventListener("click", function(){
     dropdown.classList.toggle('ativo');
@@ -32,25 +36,47 @@ home.addEventListener("click", function(){
 })
 
 dropdownOption1.addEventListener("click", function(){
-    homeA.classList.add('ativado');
-    homeB.classList.add('none3');
-    homeA.classList.remove('none3');
+    homeAA.classList.add('ativado');
+    homeBB.classList.add('none3');
+    homeAA.classList.remove('none3');
 })
 
 dropdownOption2.addEventListener("click", function(){
-    homeA.classList.add('none3');
-    homeB.classList.add('ativado');
-    homeB.classList.remove('none3');
+    homeAA.classList.add('none3');
+    homeBB.classList.add('ativado');
+    homeBB.classList.remove('none3');
 })
 
 botao.addEventListener("click", function myFunction() {
     const x = document.getElementById("numero").value;
     if( x == "" || x == 0){
-        homeA.innerHTML = "<div class=" + "flex iten" + ">" + "<p class=" + "circulo" + ">" + 1 + "</p><p class=" + "item" + ">" + "Item A" + 1 + "</p></div>" 
-    } else {
+        input.classList.remove('errado')
+        homeA.innerHTML = "<div class=" + "flex iten" + ">" + "<p class=" + "circulo" + ">" + 1 + "</p><p class=" + "item" + ">" + "Item A" + 1 + "</p></div>";
+    } else if ( x <= 3){
+        homeA.innerHTML = ""
     for(i = 1; i <= x ; i++){
+        input.classList.remove('errado')
         var resultado = "<div class=" + "flex iten" + ">" + "<p class=" + "circulo" + ">" + i + "</p><p class=" + "item" + ">" + "Item A" + i + "</p></div>"
-        console.log(resultado);
         homeA.innerHTML += resultado;
-    }}
+    } 
+    } else {
+        input.classList.add('errado')
+    }
+  })
+
+  botao.addEventListener("click", function myFunction() {
+    const x = document.getElementById("numero").value;
+    if( x == "" || x == 0){
+        input.classList.remove('errado')
+        homeB.innerHTML = "<div class=" + "flex iten" + ">" + "<p class=" + "circulo" + ">" + 1 + "</p><p class=" + "item" + ">" + "Item B" + 1 + "</p></div>";
+    } else if ( x <= 3){
+        homeB.innerHTML = ""
+    for(i = 1; i <= x ; i++){
+        input.classList.remove('errado')
+        var resultado = "<div class=" + "flex iten" + ">" + "<p class=" + "circulo" + ">" + i + "</p><p class=" + "item" + ">" + "Item B" + i + "</p></div>"
+        homeB.innerHTML += resultado;
+    } 
+    } else {
+        input.classList.add('errado')
+    }
   })
